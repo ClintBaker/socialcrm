c5e5daaa7a5fc893eeb2716561b8bb31651d35d5
Clint Baker <clintjbaker@gmail.com>
2024-03-21 15:24:42 -0600

    get starters route

--- server/routers/starterRouter.ts
+++ server/routers/starterRouter.ts
@@ -3,6 +3,6 @@ import { createStarter, getStarters } from '../handlers/starter'
 
 export const starterRouter = Router()
 
-starterRouter.get('/:connectionId', getStarters)
+starterRouter.get('/:connectionId/starters', getStarters)
 starterRouter.post('/:connectionId', createStarter)
 // starterRouter.get('/:connectionId/:starterId')
