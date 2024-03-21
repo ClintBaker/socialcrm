b664be8d27bbd0d8f3722654518c68d5b95a2541
Clint Baker <clintjbaker@gmail.com>
2024-03-21 15:50:32 -0600

    delete starter

--- server/handlers/starter.ts
+++ server/handlers/starter.ts
@@ -91,3 +91,31 @@ export const editStarter: RequestHandler = async (req: any, res, next) => {
     return next(e)
   }
 }
+
+export const deleteStarter: RequestHandler = async (req: any, res, next) => {
+  try {
+    // validate the user owns the connection
+    const connection = await prisma.connection.findUnique({
+      where: {
+        id: Number(req.params.connectionId),
+        userId: req.auth.id,
+      },
+    })
+    // if we can't find the connection, either there is no connection or the user doesn't own it
+    if (!connection) {
+      res.status(400)
+      return next(new Error('unauthorized, or connection does not exist'))
+    }
+    // delete starter
+    const deletedStarter = await prisma.starter.delete({
+      where: {
+        id: Number(req.params.starterId),
+        connectionId: Number(req.params.connectionId),
+      },
+    })
+    res.status(200).send({ starter: deletedStarter })
+  } catch (e) {
+    res.status(500)
+    return next(e)
+  }
+}
--- server/routers/starterRouter.ts
+++ server/routers/starterRouter.ts
@@ -1,9 +1,15 @@
 import { Router } from 'express'
-import { createStarter, editStarter, getStarters } from '../handlers/starter'
+import {
+  createStarter,
+  deleteStarter,
+  editStarter,
+  getStarters,
+} from '../handlers/starter'
 
 export const starterRouter = Router({ mergeParams: true })
 
 starterRouter.get('/', getStarters)
 starterRouter.post('/', createStarter)
 starterRouter.put('/:starterId', editStarter)
+starterRouter.delete('/:starterId', deleteStarter)
 // starterRouter.get('/:connectionId/:starterId')
