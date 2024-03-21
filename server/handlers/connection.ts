a97499a0cf3ef5c4a08e228d72006f349d8a6405
Clint Baker <clintjbaker@gmail.com>
2024-03-21 13:56:58 -0600

    edit connection

--- server/handlers/connection.ts
+++ server/handlers/connection.ts
@@ -26,11 +26,30 @@ export const createConnection: RequestHandler = async (req: any, res, next) => {
       },
     })
 
-    console.log(newConnection)
     res.status(201).send({ connection: newConnection })
   } catch (e) {
     console.dir(e)
     res.status(500)
     next(new Error('unable to create connection'))
+  }
+}
+
+export const editConnection: RequestHandler = async (req: any, res, next) => {
+  try {
+    // find and edit connection where the userId matches req.auth.id & connection id matches params
+    const editedConnection = await prisma.connection.update({
+      where: {
+        id: Number(req.params.connectionId),
+        userId: req.auth.id,
+      },
+      data: {
+        ...req.body,
+      },
+    })
+    res.status(200).send({ connection: editedConnection })
+  } catch (e) {
+    console.log(e)
+    res.status(500)
+    next(new Error('Unable to edit connection'))
   }
 }
--- server/routers/connectionRouter.ts
+++ server/routers/connectionRouter.ts
@@ -1,8 +1,13 @@
 import { Router } from 'express'
 import { login, signup } from '../modules/auth'
-import { createConnection, getConnections } from '../handlers/connection'
+import {
+  createConnection,
+  editConnection,
+  getConnections,
+} from '../handlers/connection'
 
 export const connectionRouter = Router()
 
 connectionRouter.get('/', getConnections)
 connectionRouter.post('/', createConnection)
+connectionRouter.put('/:connectionId', editConnection)
