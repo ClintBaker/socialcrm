67960c510238426fb1520d6e017d39e30561d191
Clint Baker <clintjbaker@gmail.com>
2024-03-21 14:20:13 -0600

    get one connection

--- server/handlers/connection.ts
+++ server/handlers/connection.ts
@@ -16,6 +16,23 @@ export const getConnections: RequestHandler = async (req: any, res, next) => {
   }
 }
 
+export const getOneConnection: RequestHandler = async (req: any, res, next) => {
+  try {
+    // get connection that matches param id and req.auth.id
+    const connection = await prisma.connection.findUnique({
+      where: {
+        id: Number(req.params.connectionId),
+        userId: req.auth.id,
+      },
+    })
+
+    res.status(200).send({ connection })
+  } catch (e) {
+    res.status(500)
+    next(new Error('unable to get connection'))
+  }
+}
+
 export const createConnection: RequestHandler = async (req: any, res, next) => {
   try {
     // create a connection with req.body and attach user id from auth.id
