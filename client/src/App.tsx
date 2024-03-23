749c1271990364ea7e5c4a4d79f0b40920b37ed8
Clint Baker <clintjbaker@gmail.com>
2024-03-22 20:43:33 -0600

    add landing page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,12 @@
 import { Route, Routes } from 'react-router-dom'
+import Landing from './components/Landing'
 
 function App() {
   return (
     <>
       <h1>App</h1>
       <Routes>
-        <Route path="/" element={<div>HELLO</div>} />
+        <Route path="/" element={<Landing />} />
       </Routes>
     </>
   )
