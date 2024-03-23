386418819f9971021135366a2c7356e7e67a046c
Clint Baker <clintjbaker@gmail.com>
2024-03-22 18:13:17 -0600

    react router dom established

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,12 @@
+import { Route, Routes } from 'react-router-dom'
+
 function App() {
   return (
     <>
       <h1>App</h1>
+      <Routes>
+        <Route path="/" element={<div>HELLO</div>} />
+      </Routes>
     </>
   )
 }
