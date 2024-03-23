ac56bda49d462973c0c35f1f09eb5abfc8c1b053
Clint Baker <clintjbaker@gmail.com>
2024-03-23 14:04:42 -0600

    add scrolltotop functionality

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,19 +3,22 @@ import Landing from './components/landing/Landing'
 import Nav from './components/utility/Nav'
 import Footer from './components/utility/Footer'
 import Auth from './components/auth/Auth'
+import ScrollToTop from './components/utility/ScrollToTop'
 
 function App() {
   return (
     <>
       <Nav />
       <main className="bg-white h-screen">
-        <Routes>
-          <Route path="/" element={<Landing />} />
-          <Route path="/auth" element={<Auth />} />
+        <ScrollToTop>
+          <Routes>
+            <Route path="/" element={<Landing />} />
+            <Route path="/auth" element={<Auth />} />
 
-          {/* 404 */}
-          <Route path="*" element={<Navigate to="/" />} />
-        </Routes>
+            {/* 404 */}
+            <Route path="*" element={<Navigate to="/" />} />
+          </Routes>
+        </ScrollToTop>
       </main>
       <Footer />
     </>
--- client/src/components/utility/Nav.tsx
+++ client/src/components/utility/Nav.tsx
@@ -1,14 +1,20 @@
+import { Link } from 'react-router-dom'
+
 export default function Nav() {
   return (
     <nav className="bg-white p-4 font-semibold border-b-2 flex  items-center">
-      <div className="h-12 w-12 rounded-xl overflow-hidden mr-2">
-        <img
-          src="REL-M-4.jpeg"
-          alt="Profile Picture"
-          className="object-cover w-full h-full"
-        />
-      </div>
-      <h1 className="text-3xl text-gray-900">REL-M</h1>
+      <Link to="/">
+        <div className="flex items-center">
+          <div className="h-12 w-12 rounded-xl overflow-hidden mr-2">
+            <img
+              src="REL-M-4.jpeg"
+              alt="Profile Picture"
+              className="object-cover w-full h-full"
+            />
+          </div>
+          <h1 className="text-3xl text-gray-900">REL-M</h1>
+        </div>
+      </Link>
     </nav>
   )
 }
